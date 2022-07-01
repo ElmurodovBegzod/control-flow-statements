9f0de187e2b45140b9ec83f7e1398eb814baee19
Begzod <ebegzod477@gmail.com>
2022-07-01 14:57:06 +0500

    code014

---
+++ bin/if01.dart
@@ -0,0 +1,23 @@
+/*
+    Create function called func
+    Create a function argument  called number of type int
+    If the number is positive, increase it to 1, else leave unchanged.
+    Args:
+        a: integer
+    Returns:
+        a: a increased by 1 if positive, else unchanged.
+ */
+ int func(int number){
+    int r=0;
+    if(number>0){
+        r=number + 1;
+    }
+
+    if(number<0){
+       r= number;
+    }
+    return r;
+ }
+void main() {
+    print(func( - 1));
+}
